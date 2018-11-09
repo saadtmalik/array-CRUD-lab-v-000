12db1277661cb33b45266d2301768a1f24140dd6
Saad Malik <saad.t.malik@gmail.com>
2018-11-09 20:49:43 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["A","B","C","D","arrays!"]
-  array.pop.("arrays!")
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
