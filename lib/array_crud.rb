05061f5b766d0b5712b0eb2e9ab86ef786e617f4
Saad Malik <saad.t.malik@gmail.com>
2018-11-09 20:54:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,11 +25,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.first
+  array[0]
 
 end
 
 def retrieve_first_element_from_array(array)
+  array.first
 
 end
 
