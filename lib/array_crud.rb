13a53c2e84ccbf0636ff06c0a1e855511bf4d826
Saad Malik <saad.t.malik@gmail.com>
2018-11-09 20:49:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["A","B","C","D"]
+  array = ["A","B","C","D","arrays!"]
   array << "arrays!"
 end
 
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["A","B","C","D","arrays!"]
+  array.pop.("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
